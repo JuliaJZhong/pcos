ace7942d4f4b8d6e760b2ece0bb70f5ca668cdcc
Julia Zhong <juliazhongg@gmail.com>
2025-05-07 15:40:24 -0400

    some cleaning

--- tensor_decomp/src/gsea.py
+++ tensor_decomp/src/gsea.py
@@ -30,14 +30,15 @@ gene_factors = np.array(adata.varm['Pf2_C'])
 # 26 (visually seems to correlate with 721 B lymphoblast)
 # 16 (largest magnitude from logistic regression with lasos regularization)
 # component = np.argmax(adata.uns['Pf2_weights'])
-component = 26
+component = 16
 
 # list of genes and their corresponding factor values for above selected component
 ranked_genes = pd.DataFrame()
 ranked_genes['gene_symbols'] = adata.var_names.values
-ranked_genes['latent_factors'] = gene_factors[:, component]
+ranked_genes['latent_factors'] = gene_factors[:, component] # TODO
+ranked_genes.sort_values(by='latent_factors', ascending=False, inplace=True)
 
-# path to the full C2 GMT file you downloaded
+# path to the full C2 GMT file downloaded
 c2_gmt_file = data_path + '/c2.all.v2024.1.Hs.symbols.gmt'
 
 # load the gene sets
@@ -56,7 +57,7 @@ for name in filtered_gene_sets:
 pre_res = gp.prerank(
     rnk=ranked_genes,                                   # dataframe of genes and scores    
     gene_sets=filtered_gene_sets,                       # your custom gene set file
-    outdir= fig_path + '/GSEA_' + str(component),                         # output folder
+    outdir= fig_path + '/GSEA_' + str(component),                # output folder
     min_size=5,
     max_size=500,
     permutation_num=1000,                              
@@ -69,5 +70,5 @@ pre_res = gp.prerank(
 results = pd.read_csv(fig_path + '/GSEA_' + str(component) + '/gseapy.gene_set.prerank.report.csv')
 results_5 = results[results['FDR q-val'] < 0.05][['Term', 'NES', 'FDR q-val']]
 results_10 = results[results['FDR q-val'] < 0.1][['Term', 'NES', 'FDR q-val']]
-results_5.to_csv(data_path + '/gsea_' + str(component) + '_FDR_0.05.csv', index=False)
-results_10.to_csv(data_path + '/gsea_' + str(component) + '_FDR_0.1.csv', index=False)
+results_5.to_csv(data_path + '/GSEA_sig_pathways/gsea_' + str(component) + '_FDR_0.05.csv', index=False)
+results_10.to_csv(data_path + '/GSEA_sig_pathways/gsea_' + str(component) + '_FDR_0.1.csv', index=False)
